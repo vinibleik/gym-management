65715e1f0f915b7b70af4cefc8df2c0fd563c86d
paradoxo <vinicius_baggio@hotmail.com>
2025-04-05 00:44:05 -0300

    fix: validate_dataclass decorator

--- backend/src/utils.py
+++ backend/src/utils.py
@@ -1,70 +1,112 @@
 import json
-from collections.abc import Iterable
-from dataclasses import fields
+from dataclasses import dataclass, fields, is_dataclass
 from typing import Any, TypedDict
 
-_POST_INIT_NAME = "__post_init__"
+_INIT_NAME = "__init__"
 
 
-class FieldError(TypedDict):
-    field: str
+class FieldErrorDict(TypedDict):
+    name: str
     value: Any
-    msg: str
+    reason: str | None
 
 
-class ValidationError(Exception): ...
+@dataclass
+class FieldError(Exception):
+    name: str
+    value: Any
+    reason: str | None = None
+
+    def to_dict(self) -> FieldErrorDict:
+        return {"name": self.name, "value": self.value, "reason": self.reason}
+
+    def to_json(self) -> str:
+        return json.dumps(self.to_dict())
+
+    def __str__(self) -> str:
+        _repr = f"{self.__class__.__name__}: Invalid value <{self.value}> for field <{self.name}>"
+        if self.reason:
+            _repr += "\n  " + f"Reason: {self.reason}"
+        return _repr
+
+    def __repr__(self) -> str:
+        if self.reason:
+            return f'{self.__class__.__name__}({self.name}, {self.value}, "{self.reason}")'
+        return f"{self.__class__.__name__}({self.name}, {self.value})"
 
 
-class FieldsValidationError(ValidationError):
-    errors: tuple[FieldError, ...]
+class ValidationErrorDict(TypedDict):
+    cls_name: str
+    errors: list[FieldErrorDict]
 
-    def __init__(self, errors: Iterable[FieldError], *args: object) -> None:
-        self.errors = tuple(errors)
-        super().__init__(self._repr_msg(), *args)
 
-    def _repr_msg(self) -> str:
-        return json.dumps(self.errors)
+@dataclass
+class ValidationError(Exception):
+    cls_name: str
+    errors: list[FieldError]
+
+    def to_dict(self) -> ValidationErrorDict:
+        return {
+            "cls_name": self.cls_name,
+            "errors": [e.to_dict() for e in self.errors],
+        }
+
+    def to_json(self) -> str:
+        return json.dumps(self.to_dict())
+
+    def __str__(self) -> str:
+        errors_str = "\n".join(map(str, self.errors))
+        return f"""\
+{self.__class__.__name__}: Error while validating {self.cls_name}. The following errors were found:
+{errors_str}"""
+
+    def __repr__(self) -> str:
+        errors_str = ",\n    ".join(repr(e) for e in self.errors)
+        _repr = f"""\
+{self.__class__.__name__}(
+  "{self.cls_name}",
+  [
+    {errors_str}
+  ]
+)"""
+        return _repr
+
+
+def is_dataclass_class(cls) -> bool:
+    return is_dataclass(cls) and isinstance(cls, type)
+
+
+def _validator_name(field_name: str) -> str:
+    return f"__validate_{field_name}__"
+
+
+def validate_fields(self):
+    errors: list[FieldError] = []
+    for field in fields(self):
+        name = field.name
+        value = getattr(self, name)
+        validator = getattr(self, _validator_name(name), None)
+
+        try:
+            if validator and not validator(name=name, value=value):
+                raise FieldError(name=name, value=value)
+        except FieldError as f:
+            errors.append(f)
+    if errors:
+        raise ValidationError(cls_name=self.__class__.__name__, errors=errors)
 
 
 def validate_dataclass(cls):
-    _post_init_func = getattr(cls, _POST_INIT_NAME, None)
+    if not is_dataclass_class(cls):
+        raise ValueError(f"class {cls.__name__} must be a dataclass")
 
-    def dataclass_validation(self, *args, **kwargs):
-        # Call the __post_init__ for the class before validation
-        if _post_init_func:
-            try:
-                _post_init_func(self, *args, **kwargs)
-            except Exception as e:
-                raise ValidationError(
-                    f"Error with post initializing class {cls.__name__}"
-                ) from e
+    cls__init__ = getattr(cls, _INIT_NAME, None)
 
-        errors: list[FieldError] = []
-        for field in fields(self):
-            name = field.name
-            value = getattr(self, name)
-            try:
-                validator = getattr(self, f"__validate_{name}__", None)
+    def validate__init__(self, *args, **kwargs):
+        if cls__init__:
+            cls__init__(self, *args, **kwargs)
+        validate_fields(self)
 
-                if validator is not None and not validator(value):
-                    errors.append(
-                        {
-                            "field": name,
-                            "value": value,
-                            "msg": f"Invalid value {value} for field {name}",
-                        }
-                    )
-            except Exception as e:
-                errors.append(
-                    {
-                        "field": name,
-                        "value": value,
-                        "msg": str(e),
-                    }
-                )
-        if errors:
-            raise FieldsValidationError(errors)
-
-    setattr(cls, _POST_INIT_NAME, dataclass_validation)
+    setattr(cls, _INIT_NAME, validate__init__)
 
     return cls
--- backend/tests/unit/test_utils.py
+++ backend/tests/unit/test_utils.py
@@ -1,174 +1,441 @@
 import json
 import unittest
-from dataclasses import InitVar, dataclass
-from datetime import date
+from dataclasses import dataclass
 from typing import Any
 from unittest.mock import MagicMock
 
-from src.utils import (
-    FieldsValidationError,
-    ValidationError,
-    validate_dataclass,
-)
+from src.utils import FieldError, ValidationError, validate_dataclass
 
 
-class TestPostInitValidation(unittest.TestCase):
-    def test_call_post_init(self):
-        @dataclass
-        @validate_dataclass
-        class TestUser:
-            name: str
-            last_name: InitVar[str]
+class TestFieldError(unittest.TestCase):
+    error_no_reason: FieldError
+    error_reason: FieldError
+    name = "name"
+    value = "value"
+    reason = "reason"
 
-            def __post_init__(self, last_name: str):
-                self.name = self.name.upper() + last_name
+    def setUp(self) -> None:
+        self.error_no_reason = FieldError(self.name, self.value)
+        self.error_reason = FieldError(self.name, self.value, self.reason)
 
-        name = "user"
-        last_name = "name"
-        user = TestUser(name, last_name)
+    def test_field_error_init(self):
+        f = FieldError(self.name, self.value)
+        self.assertEqual(f.name, self.name)
+        self.assertEqual(f.value, self.value)
+        self.assertEqual(f.reason, None)
+
+        f = FieldError(name=self.name, value=self.value, reason=self.reason)
+        self.assertEqual(f.name, self.name)
+        self.assertEqual(f.value, self.value)
+        self.assertEqual(f.reason, self.reason)
+
+    def test_repr_with_no_reason(self):
         self.assertEqual(
-            name.upper() + last_name,
-            user.name,
-            "post_init_validation should call the __post_init__ method defined in class",
+            repr(self.error_no_reason),
+            f"{FieldError.__name__}({self.name}, {self.value})",
         )
 
-    def test_raise_post_init(self):
-        @dataclass
-        @validate_dataclass
-        class TestUser:
-            def __post_init__(self):
-                raise Exception()
+    def test_repr_with_reason(self):
+        self.assertEqual(
+            repr(self.error_reason),
+            f'{FieldError.__name__}({self.name}, {self.value}, "{self.reason}")',
+        )
+
+    def test_str_with_no_reason(self):
+        self.assertEqual(
+            str(self.error_no_reason),
+            f"{FieldError.__name__}: Invalid value <{self.value}> for field <{self.name}>",
+        )
+
+    def test_str_with_reason(self):
+        self.assertEqual(
+            str(self.error_reason),
+            f"{FieldError.__name__}: Invalid value <{self.value}> for field <{self.name}>\n  Reason: {self.reason}",
+        )
+
+    def test_should_return_dict_with_reason(self):
+        self.assertEqual(
+            self.error_reason.to_dict(),
+            {"name": self.name, "value": self.value, "reason": self.reason},
+        )
+
+    def test_should_return_dict_with_no_reason(self):
+        self.assertEqual(
+            self.error_no_reason.to_dict(),
+            {"name": self.name, "value": self.value, "reason": None},
+        )
+
+    def test_should_return_json_str_with_reason(self):
+        self.assertEqual(
+            self.error_reason.to_json(),
+            json.dumps(
+                {"name": self.name, "value": self.value, "reason": self.reason}
+            ),
+        )
+
+    def test_should_return_json_str_with_no_reason(self):
+        self.assertEqual(
+            self.error_no_reason.to_json(),
+            json.dumps(
+                {"name": self.name, "value": self.value, "reason": None}
+            ),
+        )
+
+
+class TestValidateError(unittest.TestCase):
+    error_no_reason: FieldError
+    error_reason: FieldError
+    errors_tests: list[list[FieldError]]
+    name = "name"
+    value = "value"
+    reason = "reason"
+    cls_name = "cls_name"
+
+    def setUp(self) -> None:
+        self.error_no_reason = FieldError(self.name, self.value)
+        self.error_reason = FieldError(self.name, self.value, self.reason)
+        self.errors_tests = [
+            [self.error_reason],
+            [self.error_reason, self.error_no_reason],
+        ]
+
+    def test_field_error_init(self):
+        v = ValidationError(
+            self.cls_name, [self.error_reason, self.error_no_reason]
+        )
+        self.assertEqual(v.cls_name, self.cls_name)
+        self.assertListEqual(
+            v.errors, [self.error_reason, self.error_no_reason]
+        )
+
+    def test_validate_to_dict(self):
+        for i, errors in enumerate(self.errors_tests):
+            with self.subTest(i=i):
+                v = ValidationError(self.cls_name, errors)
+                self.assertDictEqual(
+                    v.to_dict(),
+                    {
+                        "cls_name": self.cls_name,
+                        "errors": [e.to_dict() for e in errors],
+                    },
+                )
+
+    def test_validate_to_json(self):
+        for i, errors in enumerate(self.errors_tests):
+            with self.subTest(i=i):
+                v = ValidationError(self.cls_name, errors)
+                self.assertEqual(
+                    v.to_json(),
+                    json.dumps(
+                        {
+                            "cls_name": self.cls_name,
+                            "errors": [e.to_dict() for e in errors],
+                        }
+                    ),
+                )
+
+    def test_validate_to_str(self):
+        for i, errors in enumerate(self.errors_tests):
+            v = ValidationError(self.cls_name, errors)
+            expected = f"{ValidationError.__name__}: Error while validating {self.cls_name}. The following errors were found:\n{'\n'.join(map(str, errors))}"
+            with self.subTest(i=i):
+                self.assertEqual(str(v), expected)
+
+    def test_validate_to_repr(self):
+        for i, errors in enumerate(self.errors_tests):
+            v = ValidationError(self.cls_name, errors)
+            expected = f'{ValidationError.__name__}(\n  "{self.cls_name}",\n  [\n    {",\n    ".join(map(repr, errors))}\n  ]\n)'
+            with self.subTest(i=i):
+                self.assertEqual(repr(v), expected)
+
+
+class TestValidateDataclass(unittest.TestCase):
+    def test_should_not_throw_error_for_not_dataclass(self):
+        with self.assertRaisesRegex(
+            ValueError, "class Person must be a dataclass"
+        ):
+
+            @validate_dataclass
+            class Person: ...
 
         with self.assertRaisesRegex(
-            ValidationError, "Error with post initializing class TestUser"
+            ValueError, "class Student must be a dataclass"
         ):
-            TestUser()
 
-    def test_call_validations(self):
+            @validate_dataclass
+            class Student: ...
+
+        @validate_dataclass
+        @dataclass
+        class ShouldWork: ...
+
+    def test_should_run_fields_validators(self):
         name_validation = MagicMock(return_value=True)
         age_validation = MagicMock(return_value=True)
 
-        @dataclass
         @validate_dataclass
-        class TestUser:
+        @dataclass
+        class Person:
             name: str
             age: int
 
-            def __validate_name__(*args):
-                return name_validation(*args)
+            def __validate_name__(*args, **kwargs):
+                return name_validation(*args, **kwargs)
 
-            def __validate_age__(*args):
-                return age_validation(*args)
+            def __validate_age__(*args, **kwargs):
+                return age_validation(*args, **kwargs)
 
         name = "user"
         age = 23
-        user = TestUser(name, age)
+        user = Person(name, age)
 
-        name_validation.assert_called_once_with(user, name)
-        age_validation.assert_called_once_with(user, age)
+        name_validation.assert_called_once_with(user, name="name", value=name)
+        age_validation.assert_called_once_with(user, name="age", value=age)
 
-    def test_raise_one_validations_errors(self):
-        @dataclass
+    def test_should_run_fields_validators_inheritance(self):
+        name_validation = MagicMock(return_value=True)
+        person_age_validation = MagicMock(return_value=True)
+        student_age_validation = MagicMock(return_value=True)
+        id_validation = MagicMock(return_value=True)
+
         @validate_dataclass
-        class TestUser:
-            name: str
-
-            def __validate_name__(*args):
-                return False
-
-        name = "user"
-        errors = (
-            {
-                "field": "name",
-                "value": "user",
-                "msg": "Invalid value user for field name",
-            },
-        )
-
-        with self.assertRaisesRegex(
-            FieldsValidationError, json.dumps(errors)
-        ) as e:
-            TestUser(name)
-        self.assertTupleEqual(
-            e.exception.errors,
-            errors,
-        )
-
-    def test_raise_multiple_validations_errors(self):
         @dataclass
-        @validate_dataclass
-        class TestUser:
+        class Person:
             name: str
             age: int
 
-            def __validate_name__(*args):
-                return False
+            def __validate_name__(*args, **kwargs):
+                return name_validation(*args, **kwargs)
 
-            def __validate_age__(*args):
-                return False
+            def __validate_age__(*args, **kwargs):
+                return person_age_validation(*args, **kwargs)
+
+        @validate_dataclass
+        @dataclass
+        class Student(Person):
+            id: str
+
+            def __validate_age__(*args, **kwargs):
+                return student_age_validation(*args, **kwargs)
+
+            def __validate_id__(*args, **kwargs):
+                return id_validation(*args, **kwargs)
 
         name = "user"
         age = 23
+        _id = "id"
+        user = Student(name, age, _id)
 
-        errors = (
-            {
-                "field": "name",
-                "value": "user",
-                "msg": "Invalid value user for field name",
-            },
-            {
-                "field": "age",
-                "value": 23,
-                "msg": "Invalid value 23 for field age",
-            },
+        name_validation.assert_called_once_with(user, name="name", value=name)
+        person_age_validation.assert_not_called()
+        student_age_validation.assert_called_once_with(
+            user, name="age", value=age
         )
+        id_validation.assert_called_once_with(user, name="id", value=_id)
+
+    def test_should_run_fields_validators_inheritance_and_init(self):
+        name_validation = MagicMock(return_value=True)
+        person_age_validation = MagicMock(return_value=True)
+        student_age_validation = MagicMock(return_value=True)
+        id_validation = MagicMock(return_value=True)
+
+        @validate_dataclass
+        @dataclass
+        class Person:
+            name: str
+            age: int
+
+            def __validate_name__(*args, **kwargs):
+                return name_validation(*args, **kwargs)
+
+            def __validate_age__(*args, **kwargs):
+                return person_age_validation(*args, **kwargs)
+
+        @validate_dataclass
+        @dataclass
+        class Student(Person):
+            id: str
+
+            def __init__(self, _id, name, age) -> None:
+                self.id = age
+                self.name = _id
+                self.age = name
+
+            def __validate_age__(*args, **kwargs):
+                return student_age_validation(*args, **kwargs)
+
+            def __validate_id__(*args, **kwargs):
+                return id_validation(*args, **kwargs)
+
+        name = "user"
+        age = 23
+        _id = "id"
+        s = Student(_id=_id, name=name, age=age)
+
+        self.assertEqual(s.id, age)
+        self.assertEqual(s.name, _id)
+        self.assertEqual(s.age, name)
+        name_validation.assert_called_once_with(s, name="name", value=_id)
+        person_age_validation.assert_not_called()
+        student_age_validation.assert_called_once_with(
+            s, name="age", value=name
+        )
+        id_validation.assert_called_once_with(s, name="id", value=age)
+
+    def test_should_throw_error_for_failed_validator(self):
+        @validate_dataclass
+        @dataclass
+        class Person:
+            name: str
+
+            def __validate_name__(self, name: str, value: str) -> bool:
+                return False
 
         with self.assertRaisesRegex(
-            FieldsValidationError, json.dumps(errors)
+            ValidationError,
+            f"{ValidationError.__name__}: Error while validating {Person.__name__}",
         ) as e:
-            TestUser(name, age)
-        self.assertTupleEqual(e.exception.errors, errors)
+            Person("")
 
-    def test_correct_parse_fields(self):
-        @dataclass
+        exc = e.exception
+        self.assertEqual(exc.cls_name, Person.__name__)
+        self.assertEqual(len(exc.errors), 1)
+        name = exc.errors[0]
+        self.assertEqual(name.to_json(), FieldError("name", "").to_json())
+
+    def test_should_throw_error_for_failed_validator_inhertance(self):
+        name = "name"
+        age = 23
+
         @validate_dataclass
-        class Test:
+        @dataclass
+        class Person:
+            name: str
+
+            def __validate_name__(self, name: str, value: str) -> bool:
+                return False
+
+        @validate_dataclass
+        @dataclass
+        class Student(Person):
+            def __validate_name__(self, name: str, value: str) -> bool:
+                return True
+
+        Student(name)
+
+        @validate_dataclass
+        @dataclass
+        class Employee(Person):
             age: int
-            birth: date
 
-            def __post_init__(self) -> None:
-                self.age = int(self.age)
+            def __validate_age__(self, name: str, value: int) -> bool:
+                return False
 
-            def __validate_birth__(self, birth: Any) -> bool:
-                if isinstance(birth, date):
-                    return True
-                self.birth = date.fromisoformat(birth)
-                return True
+        with self.assertRaisesRegex(
+            ValidationError,
+            f"{ValidationError.__name__}: Error while validating {Employee.__name__}",
+        ) as e:
+            Employee(name, age)
 
-        birth = "1999-01-01"
-        age = "23"
-        t = Test(age, birth)  # type: ignore
-        self.assertIsInstance(t.age, int)
-        self.assertEqual(t.age, 23)
-        self.assertIsInstance(t.birth, date)
-        self.assertEqual(t.birth, date(year=1999, month=1, day=1))
+        exc = e.exception
+        self.assertEqual(exc.cls_name, Employee.__name__)
+        self.assertEqual(len(exc.errors), 2)
+        name_error, age_error = exc.errors
+        self.assertEqual(
+            name_error.to_json(), FieldError("name", name).to_json()
+        )
+        self.assertEqual(age_error.to_json(), FieldError("age", age).to_json())
 
-    def test_capture_exception_on_validate(self):
-        @dataclass
+    def test_should_throw_error_with_custom_errors(self):
+        name = "name"
+        name_error_msg = "name should not be empty"
+        error_name = FieldError("name", name, name_error_msg)
+        age = 23
+        age_error_msg = "age should not be 23"
+        error_age = FieldError("age", age, age_error_msg)
+
         @validate_dataclass
-        class Test:
-            birth: date
+        @dataclass
+        class Person:
+            name: str
 
-            def __validate_birth__(self, birth: Any) -> bool:
-                if isinstance(birth, date):
-                    return True
-                self.birth = date.fromisoformat(birth)
+            def __validate_name__(self, name: str, value: str) -> bool:
+                raise error_name
+
+        @validate_dataclass
+        @dataclass
+        class Student(Person):
+            age: int
+
+            def __validate_age__(self, name: str, value: Any) -> bool:
+                raise error_age
+
+        with self.assertRaisesRegex(
+            ValidationError,
+            f"{ValidationError.__name__}: Error while validating {Student.__name__}",
+        ) as e:
+            Student(name, age)
+
+        exc = e.exception
+        self.assertEqual(exc.cls_name, Student.__name__)
+        errors = exc.errors
+        self.assertEqual(len(errors), 2)
+        name_err, age_err = errors
+        self.assertEqual(name_err.to_json(), error_name.to_json())
+        self.assertEqual(age_err.to_json(), error_age.to_json())
+
+        @validate_dataclass
+        @dataclass
+        class Employee(Student):
+            age: int
+
+            def __validate_age__(self, name: str, value: Any) -> bool:
                 return True
 
-        birth = "2026-00-00"
-        with self.assertRaises(FieldsValidationError) as e:
-            Test(birth)  # type: ignore
+        with self.assertRaisesRegex(
+            ValidationError,
+            f"{ValidationError.__name__}: Error while validating {Employee.__name__}",
+        ) as e:
+            Employee(name, age)
 
-        self.assertEqual(len(e.exception.errors), 1)
-        self.assertEqual(e.exception.errors[0]["field"], "birth")
-        self.assertEqual(e.exception.errors[0]["value"], birth)
+        exc = e.exception
+        self.assertEqual(exc.cls_name, Employee.__name__)
+        errors = exc.errors
+        self.assertEqual(len(errors), 1)
+        name_err = errors[0]
+        self.assertEqual(name_err.to_json(), error_name.to_json())
+
+        @validate_dataclass
+        @dataclass
+        class Chef(Employee):
+            age: int
+
+            def __validate_name__(self, name: str, value: str) -> bool:
+                return True
+
+            def __validate_age__(self, name: str, value: Any) -> bool:
+                raise error_age
+
+        with self.assertRaisesRegex(
+            ValidationError,
+            f"{ValidationError.__name__}: Error while validating {Chef.__name__}",
+        ) as e:
+            Chef(name, age)
+
+        exc = e.exception
+        self.assertEqual(exc.cls_name, Chef.__name__)
+        errors = exc.errors
+        self.assertEqual(len(errors), 1)
+        age_err = errors[0]
+        self.assertEqual(age_err.to_json(), error_age.to_json())
+
+        @validate_dataclass
+        @dataclass
+        class Boss(Chef):
+            def __validate_name__(self, name: str, value: str) -> bool:
+                return True
+
+            def __validate_age__(self, name: str, value: Any) -> bool:
+                return True
+
+        Boss("", 23)
